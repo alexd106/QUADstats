bbc1562027be3c12b8cbbc28f3de76eb904bd036
alexd106 <alexd106@gmail.com>
2021-02-03 10:48:38 +0000

    hide day 1 and 3 exrecise solutions

--- reveal.R
+++ reveal.R
@@ -1,15 +1,9 @@
 # Change to TRUE to show exercise solutions and R scripts
-show_text12 <- TRUE    # ex 1 and 2
-show_text34 <- TRUE    # ex 3 and 4
-show_text5 <- TRUE    # ex 5
-show_text_GDE <- TRUE    # ex GDE
+show_text_GDE <- FALSE    # ex GDE
 show_text_lm1 <- TRUE    # ex LM1
 show_text_lm2 <- TRUE    # ex LM2
-show_text_lm3 <- TRUE   # ex LM3
-show_text_lm4 <- TRUE    # ex LM4
-show_text_glm1 <- TRUE    # ex GLM1
-show_text_glm2 <- TRUE    # ex GLM2
-show_text_glm3 <- TRUE    # ex GLM3
+show_text_lm3 <- FALSE   # ex LM3
+show_text_lm4 <- FALSE    # ex LM4
 
 # Change to TRUE to show lectures
 show_lecture <- TRUE
