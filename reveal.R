03b364b45c67e0b808dbdbb9dca22d3d22505f1b
alexd106 <alexd106@gmail.com>
2021-02-05 10:54:15 +0000

    add GDE exercise and solutions

--- reveal.R
+++ reveal.R
@@ -1,5 +1,5 @@
 # Change to TRUE to show exercise solutions and R scripts
-show_text_GDE <- FALSE    # ex GDE
+show_text_GDE <- TRUE    # ex GDE
 show_text_lm1 <- TRUE    # ex LM1
 show_text_lm2 <- TRUE    # ex LM2
 show_text_lm3 <- FALSE   # ex LM3
